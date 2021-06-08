41a1d27d08ae8f7d1cb5184633618c73dfd7ffb0
Meera Nair <murali.meera@gmail.com>
2021-06-08 15:08:58 -0500

    thymeleaf Exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,35 +2,39 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * Created by Chris Bay
+ */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+   // private static List<String> events = new ArrayList<>();
+    private static Map<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events",events);
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", events);
         return "events/index";
     }
 
-    //lives in /events/create
-    @GetMapping("create")
-    public String renderCreateEventForm(){
+    @PostMapping("/events/create")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    //lives at /events/create
-    @PostMapping("create")
-    public String CreateEvent(@RequestParam String eventName){
-        events.add(eventName);
+    @GetMapping ("create")
+    //@RequestMapping(value = "/event/create", method = RequestMethod.GET)
+    public String processCreateEventForm(@RequestParam(value="eventName") String eventName,@RequestParam(value="description") String description) {
+        events.put(eventName,description);
         return "redirect:";
     }
+
 }
+
