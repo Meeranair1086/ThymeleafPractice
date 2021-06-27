34b10a0a6f9e56940cc78ad966827b19126c4749
Meera Nair <murali.meera@gmail.com>
2021-06-27 15:22:44 -0500

    initial commit for studio chapter 17

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -8,15 +8,7 @@ import java.util.Objects;
 
 
 @Entity
-public class Event {
-
-    @Id
-    @GeneratedValue
-    private int id;
-
-    @NotBlank(message = "Name is required")
-    @Size(min=3,max=50,message = "Name must be between 3 and 50 characters.")
-    private String name;
+public class Event extends AbstractEntity {
 
     @Size(max=500,message = "Description too long.")
     private String description;
@@ -37,7 +29,6 @@ public class Event {
     private EventType type;
 
     public Event(String name, String description, String contactEmail,String location,int numberOfAttendees,EventType type) {
-        this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
         this.location = location;
@@ -48,13 +39,6 @@ public class Event {
 
     public Event(){}
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getDescription() {
         return description;
@@ -72,9 +56,7 @@ public class Event {
         this.contactEmail = contactEmail;
     }
 
-    public int getId() {
-        return id;
-    }
+
 
     public String getLocation() {
         return location;
@@ -108,22 +90,6 @@ public class Event {
         this.type = type;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
 
 }
--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -9,56 +9,18 @@ import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
-public class EventCategory {
-
-    @Id
-    @GeneratedValue
-    private int id;
-
-    @NotBlank(message = "Name is required")
-    @Size(min=3,max=50,message = "Name must be between 3 and 50 characters.")
-    private String name;
-
-    public EventCategory(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
+public class EventCategory extends AbstractEntity {
     public EventCategory() {}
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        EventCategory eventCategory = (EventCategory) o;
-        return id == eventCategory.id;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 
-    @Override
-    public String toString() {
-        return name;
-    }
+
+
+
+
 
 
 
