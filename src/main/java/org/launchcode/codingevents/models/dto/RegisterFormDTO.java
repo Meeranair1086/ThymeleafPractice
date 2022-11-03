49baa8395a09344f4332016aeb7fdc1a50f9a779
Meera Nair <murali.meera@gmail.com>
2022-11-03 16:38:05 -0500

    Final commit

--- src/main/java/org/launchcode/codingevents/models/dto/RegisterFormDTO.java
+++ src/main/java/org/launchcode/codingevents/models/dto/RegisterFormDTO.java
@@ -1,2 +1,14 @@
-package org.launchcode.codingevents.models.dto;public class RegisterFormDTO {
+package org.launchcode.codingevents.models.dto;
+
+public class RegisterFormDTO extends LoginFormDTO{
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
 }
